e79356273f76082152a0d17999fa4547db51dec3
XION <igor97645@gmail.com>
2024-11-28 19:34:59 +0200

    added slippers

--- src/main/java/com/noix/terriblemod/item/ModItems.java
+++ src/main/java/com/noix/terriblemod/item/ModItems.java
@@ -5,9 +5,7 @@ import com.noix.terriblemod.block.ModBlocks;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,7 +18,9 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TerribleMod.MOD_ID);
 
     public static final RegistryObject<Item> SLIPPERS = ITEMS.register("slippers",
-            () -> new Item(new Item.Properties().durability(256)));
+            () -> new SwordItem(Tiers.STONE, 6, -1.5f,
+                    new Item.Properties().stacksTo(1).durability(256))
+    );
 
     public static final RegistryObject<Item> STRANGE_STONE = ITEMS.register("strange_stone",
             () -> new BlockItem(ModBlocks.STRANGE_STONE.get(), new Item.Properties()));
